0d8640a6d20db9878a31f92f03a5ee809274337d
Tatiana Disu <t.disu@students.rave.ac.uk>
2020-01-06 17:29:02 +0000

    Using Python Mongo DB creating Database, collection, and data.

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask, request, render_template, jsonify, redirect
 import urllib.parse
 import datetime
 import os
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
 
 ON_HEROKU = "ON_HEROKU" in os.environ
 
@@ -9,7 +14,8 @@ app = Flask(__name__, static_url_path='', static_folder="static")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print (mycol.find_one())
+    return render_template('index.html', data=mycol.find())
 
 if (not ON_HEROKU) and __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
